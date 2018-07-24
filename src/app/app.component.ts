17df7eb44e1d4c2ba251772f09893575066eb6e8
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2018-07-24 18:06:20 -0400

    Connecting app to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyAeP-yWq17z6FTBCZRnoNuT5pcvMIroiVE',
+      authDomain: 'angular-book-app.firebaseapp.com',
+      databaseURL: 'https://angular-book-app.firebaseio.com',
+      projectId: 'angular-book-app',
+      storageBucket: '',
+      messagingSenderId: '20721535563'
+    };
+    firebase.initializeApp(config);
+  }
 }
